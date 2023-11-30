d110fbb13cf536a2905781c341f4aca3becd9c14
mohamadm890 <mohamad890098@gmail.com>
2023-11-30 01:58:33 -0800

    f

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-from add_0 import add
-a = 1
-b = 2
-math = add(a, b)
-print(f"{a} + {b} = {math}")
+     from add_0 import add
+     a = 1
+     b = 2
+     print("{} + {} = {}".format(a, b, add(a, b)))
