dcbd01594bf1f408ad653803f8494394ebd949d3
mohamadm890 <mohamad890098@gmail.com>
2023-12-04 08:51:46 -0800

    f

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,41 +1,72 @@
 #include "lists.h"
+
 /**
- * palindrome - check if is palindrome with recursion
- * @l: l
- * @r: r
+ * reverse_listint - reverses a linked list
+ * @head: pointer to the first node in the list
  *
- * Return: 1 palindrome, 0 not palindrome
+ * Return: pointer to the first node in the new list
  */
-int palindrome(listint_t **l, listint_t *r)
+void reverse_listint(listint_t **head)
 {
-	int response;
+  listint_t *prev = NULL;
+  listint_t *current = *head;
+  listint_t *next = NULL;
 
-	if (r != NULL)
-	{
-		response = palindrome(l, r->next);
-		if (response != 0)
-		{
-			response = (r->n == (*l)->n);
-			*l = (*l)->next;
-			return (response);
-		}
-		return (0);
+  while (current)
+    {
+      next = current->next;
+      current->next = prev;
+      prev = current;
+      current = next;
+    }
 
-	}
-	return (1);
+  *head = prev;
 }
 
 /**
- * is_palindrome - checks if a singly linked list is a palindrome.
- * @head: head of linked list
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: double pointer to the linked list
  *
- * Return: 1 palindrome, 0 not palindrome
+ * Return: 1 if it is, 0 if not
  */
 int is_palindrome(listint_t **head)
 {
-	if (head == NULL)
+  listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
+
+  if (*head == NULL || (*head)->next == NULL)
+    return (1);
+
+  while (1)
+    {
+      fast = fast->next->next;
+      if (!fast)
 	{
-		return (0);
+	  dup = slow->next;
+	  break;
 	}
-	return (palindrome(head, *head));
+      if (!fast->next)
+	{
+	  dup = slow->next->next;
+	  break;
+	}
+      slow = slow->next;
+    }
+
+  reverse_listint(&dup);
+
+  while (dup && temp)
+    {
+      if (temp->n == dup->n)
+	{
+	  dup = dup->next;
+	  temp = temp->next;
+	}
+      else
+	return (0);
+    }
+
+  if (!dup)
+    return (1);
+
+  return (0);
 }
