d5fab615bbaa5d2c18549758983e07dfff96096a
mohamadm890 <94972216+mohamadm890@users.noreply.github.com>
2024-03-11 04:15:27 -0700

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
 // Print script that prints 3 lines
 
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+console.log('C is fun\nPython is cool\nJavascript is amazing');
